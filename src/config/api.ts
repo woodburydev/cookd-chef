65e7d8fa0849a996799ee352ae8dd1f1d959a94c
woodburydev <54697770+woodburydev@users.noreply.github.com>
2022-06-07 12:48:39 -0700

    Change Ruby Version and Server URL

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
 let prod = true;
-const prodUrl = 'http://137.184.246.148';
+const prodUrl = 'https://cookd-server-s4jip.ondigitalocean.app';
 let localUrl = 'http://localhost:3000';
 export const endpoint = prod || !__DEV__ ? prodUrl : localUrl;
